28e268aea950af4d1d3ba9ef61a006ca3f4bb057
felipe <profissionalf.andre@gmail.com>
2024-01-31 21:20:37 -0300

    feat: add basic routes and install nodemon to not reload page

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import http from 'http'
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Hello World\n')
-})
-
 const PORT = 3000
 
+
+const routes = {
+    "/": "Curso de Express API",
+    "/authors": "List of authors",
+    "/books": "List of books",
+}
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end(routes[req.url] || 'Ruta no encontrada')
+})
 
 server.listen(PORT, () => console.log('Running on http://localhost:3000/'))
