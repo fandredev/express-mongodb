f8024b897371d70e7deb3b7d1b00cd02c74cc18f
felipe <profissionalf.andre@gmail.com>
2024-02-01 23:17:11 -0300

    feat: add model book and more config settings for intellij and vscode

--- src/app.js
+++ src/app.js
@@ -1,56 +1,64 @@
 import express from 'express'
 import searchBook, {findIndexBook, listBooks} from './utils/book-search.js'
+import connectDatabase from './config/db-connect.js'
+
+const database = await connectDatabase()
+database.on('error', (error) => console.log(`Error connecting to the database: ${error}`))
+database.once('open', () => console.log(`Connected to the database`))
 
 const app = express()
-
 app.use(express.json())
+
 
 const books = listBooks()
 const PORT = 3000
 
 
 app.get('/', (req, res) => {
-    res.status(200).send('Curso de Express API')
+  res.status(200).send('Curso de Express API')
 })
 
 app.get('/authors', (req, res) => {
-    res.status(200).send('List of authors')
+  res.status(200).send('List of authors')
 })
 
 app.get('/books', (req, res) => {
-    res.status(200).json(books)
+  res.status(200).json(books)
 })
 
 app.get('/books/:id', (req, res) => {
-    const id = req.params.id
-    const book = searchBook(id)
-    if (book) {
-        res.status(200).json(book)
-    } else {
-        res.status(404).send('Book not found')
-    }
+  const id = req.params.id
+  const book = searchBook(id)
+  if (book) {
+    res.status(200).json(book)
+  } else {
+    res.status(404).send('Book not found')
+  }
 })
 
 app.post('/books', (req, res) => {
-    books.push(req.body)
-    res.status(201).send('Book created')
+  books.push(req.body)
+  res.status(201).send('Book created')
 })
 
 app.put('/books/:id', (req, res) => {
-    const id = req.params.id
-    const books = searchBook(id)
-    books.title = req.body.title
+  const id = req.params.id
+  const books = searchBook(id)
+  books.title = req.body.title
 
 
-    res.status(200).json(books)
+  res.status(200).json(books)
 })
 
 app.delete('/books/:id', (req, res) => {
-    const id = req.params.id
-    const index = findIndexBook(id)
-    books.splice(index, 1)
-    res.status(200).send('Book deleted')
+  const id = req.params.id
+  const index = findIndexBook(id)
+  books.splice(index, 1)
+  res.status(200).send('Book deleted')
 })
 
 
 app.listen(PORT, () => console.log('Running on http://localhost:3000/'))
+
+// const uri = "mongodb+srv://admin:<password>@cluster0.i7lwyms.mongodb.net/?retryWrites=true&w=majority";
+// uKGjBrslVituB2ZY
