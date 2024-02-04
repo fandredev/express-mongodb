aa4b06e99a7286e6000d6b3f098c1f8156eb7d0e
felipe <profissionalf.andre@gmail.com>
2024-02-04 15:04:39 -0300

    tests: tests ci run jest test

--- src/config/db-connect.ts
+++ src/config/db-connect.ts
@@ -1,9 +1,7 @@
 import moongose from 'mongoose';
 
 export default async function connectDatabase() {
-  const uri = process.env.MONGO_DB_ATLAS_CONNECTION as string;
-
-  await moongose.connect(uri); // Connect to the database
+  await moongose.connect('mongodb+srv://admin:uKGjBrslVituB2ZY@cluster0.i7lwyms.mongodb.net/bookstore?retryWrites=true&w=majority'); // Connect to the database
   return moongose.connection; // Return the connection
 }
 
