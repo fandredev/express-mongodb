d6e45428a3667e81f3eb687da2c8bc5330d49dc7
felipe <profissionalf.andre@gmail.com>
2024-02-04 14:52:12 -0300

    refactor: remove book-search

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import express from 'express'
-import connectDatabase from './config/db-connect.js'
-import routes from './routes/index.js'
+import express from 'express';
+import connectDatabase from './config/db-connect.js';
+import routes from './routes/index.js';
 
-const database = await connectDatabase()
-database.on('error', (error) => console.log(`Error connecting to the database: ${error}`))
-database.once('open', () => console.log(`Connected to the database`))
+const database = await connectDatabase();
+database.on('error', (error) => console.log(`Error connecting to the database: ${error}`));
+database.once('open', () => console.log('Connected to the database'));
 
-const app = express()
-routes(app)
+const app = express();
+routes(app);
 
-const PORT = 3000
+const PORT = 3000;
 
 
-app.listen(PORT, () => console.log('Running on http://localhost:3000/'))
+app.listen(PORT, () => console.log('Running on http://localhost:3000/'));
